1b14280635698a649ed9d97033bbc400c922ee9e
Mizanur Rahman <mr.milonku@gmail.com>
2021-03-10 23:57:52 +0600

    Entity base class has added

---
+++ Core/Entities/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Entity
+    {
+        public long Id { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int LastUpdatedBy { get; set; }
+
+        public DateTime LastUpdatedOn { get; set; }
+
+        public Entity()
+        {
+            CreatedOn = DateTime.UtcNow;
+            LastUpdatedOn = DateTime.UtcNow;
+        }
+    }
+}
